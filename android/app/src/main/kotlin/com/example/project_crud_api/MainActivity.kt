3f10e02dbf936380038ff2bad59d13602f9250d6
unknown <gitaranidewi1@gmail.com>
2021-04-27 10:08:43 +0800

    api commit

---
+++ android/app/src/main/kotlin/com/example/project_crud_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_crud_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:project_crud_api/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
